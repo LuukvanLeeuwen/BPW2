d14ee86c20ec67c46b1fc73392bbc7ba8e2c706f
Luuk van Leeuwen <LuukLeeuwen@gmail.com>
2019-07-07 16:03:58 +0200

    Upload game
    Hele game

---
+++ Assets/Script/PlayButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+ using UnityEngine;
+ using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
